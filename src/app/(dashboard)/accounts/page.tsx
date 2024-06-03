914688e61eb5317c30229a444dfdbbe7cac9e6eb
Daryl <darylmicah12@gmail.com>
2024-06-03 16:14:28 +0530

    data table modifications

--- src/app/(dashboard)/accounts/page.tsx
+++ src/app/(dashboard)/accounts/page.tsx
@@ -1,12 +1,43 @@
 "use client";
 
+import { DataTable } from "@/components/data-table";
 import { Button } from "@/components/ui/button";
 import { Card, CardHeader, CardTitle } from "@/components/ui/card";
-import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
 import { Plus } from "lucide-react";
+
+import { Payment, columns } from "./columns";
+
+import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
 
 const AccountsPage = () => {
   const newAccount = useNewAccount();
+
+  const data: Payment[] = [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    {
+      id: "489e1d42",
+      amount: 125,
+      status: "processing",
+      email: "a@example.com",
+    },
+    {
+      id: "465ed52f",
+      amount: 300,
+      status: "success",
+      email: "p@example.com",
+    },
+    {
+      id: "212e1d42",
+      amount: 150,
+      status: "processing",
+      email: "d@example.com",
+    },
+  ];
 
   return (
     <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
@@ -18,6 +49,12 @@ const AccountsPage = () => {
             Add New
           </Button>
         </CardHeader>
+        <DataTable
+          filterKey="email"
+          columns={columns}
+          data={data}
+          onDelete={() => {}}
+        />
       </Card>
     </div>
   );
