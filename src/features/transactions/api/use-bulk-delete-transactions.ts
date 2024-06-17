2a334e3c1dbbdfd4484593d23c405cdfe682be94
Daryl <darylmicah12@gmail.com>
2024-06-17 16:03:46 +0530

    added transactions APIs and hooks

---
+++ src/features/transactions/api/use-bulk-delete-transactions.ts
@@ -0,0 +1,34 @@
+import { client } from "@/lib/hono";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { InferRequestType, InferResponseType } from "hono";
+import { toast } from "sonner";
+
+type ResponseType = InferResponseType<
+  (typeof client.api.transactions)["bulk-delete"]["$post"]
+>;
+type RequestType = InferRequestType<
+  (typeof client.api.transactions)["bulk-delete"]["$post"]
+>["json"];
+
+export const usebBulkDeleteAccounts = () => {
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation<ResponseType, Error, RequestType>({
+    mutationFn: async (json) => {
+      const response = await client.api.transactions["bulk-delete"]["$post"]({
+        json,
+      });
+      return await response.json();
+    },
+    onSuccess: () => {
+      toast.success("Transactions Deleted.");
+      queryClient.invalidateQueries({ queryKey: ["accounts"] });
+      //ToDo: Also invalidate memory
+    },
+    onError: () => {
+      toast.error("Failed to delete transactions!");
+    },
+  });
+
+  return mutation;
+};
