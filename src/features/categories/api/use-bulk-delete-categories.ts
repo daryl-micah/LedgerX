811f70d2ad9bf48fb6a3e0001e6366dda0fc86a5
Daryl <darylmicah12@gmail.com>
2024-07-02 16:35:12 +0530

    corrected typos

--- src/features/categories/api/use-bulk-delete-categories.ts
+++ src/features/categories/api/use-bulk-delete-categories.ts
@@ -10,7 +10,7 @@ type RequestType = InferRequestType<
   (typeof client.api.categories)["bulk-delete"]["$post"]
 >["json"];
 
-export const usebBulkDeleteCategories = () => {
+export const useBulkDeleteCategories = () => {
   const queryClient = useQueryClient();
 
   const mutation = useMutation<ResponseType, Error, RequestType>({
