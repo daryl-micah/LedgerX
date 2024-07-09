301a999d13f166fe0db125a3f76b1444b453d64c
Daryl <darylmicah12@gmail.com>
2024-07-10 03:07:46 +0530

    invalidated summary in hooks

--- src/features/transactions/api/use-bulk-delete-transactions.ts
+++ src/features/transactions/api/use-bulk-delete-transactions.ts
@@ -23,7 +23,7 @@ export const useBulkDeleteTransactions = () => {
     onSuccess: () => {
       toast.success("Transactions Deleted.");
       queryClient.invalidateQueries({ queryKey: ["transactions"] });
-      //ToDo: Also invalidate memory
+      queryClient.invalidateQueries({ queryKey: ["summary"] });
     },
     onError: () => {
       toast.error("Failed to delete transactions!");
